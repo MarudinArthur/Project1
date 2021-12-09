8ac9619268cf947a18996cf0b4ba9acef31931a8
MarudinArthur <85170410+MarudinArthur@users.noreply.github.com>
2021-12-10 00:38:19 +0300

    add animation, add some game buttons, add UI elements in project

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,13 +1,29 @@
 ﻿using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     [HideInInspector] public float score = 0f;
     [HideInInspector] public bool gameOver = false;
+    public static GameManager Instance;
     public TextMeshProUGUI timerCounter;
     public TextMeshProUGUI scoreCounter;
     private float _time = 10f;
+
+    /*
+    void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+      
+        //GameObject.Find("Canvas").transform.GetChild(4).gameObject.SetActive(false);
+        Instance = this;
+        DontDestroyOnLoad(transform.gameObject);
+    }*/
 
     private void Update()
     {
@@ -43,5 +59,25 @@ public class GameManager : MonoBehaviour
     private void GameOverPopUp()
     {
         GameObject.Find("Canvas").transform.GetChild(4).gameObject.SetActive(true);
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Settings()
+    {
+        //some code
+    }
+
+    public void Exit()
+    {
+        //some code
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,17 +5,20 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float _speed = 10f;
 
     public GameObject projectilePrefab;
-    private GameManager _gameManager;
+    private GameManager gameManager;
+    public Animator animator;
 
     private float _horizontalInput;
     private float _verticalInput;
     private float _horizontalBounds = 9f;
     private float _topBound = 5f;
     private float _lowerBound = -11.7f;
+    private int _state;
 
     private void Start()
     {
-        _gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
@@ -23,7 +26,7 @@ public class PlayerController : MonoBehaviour
         _horizontalInput = Input.GetAxis("Horizontal");
         _verticalInput = Input.GetAxis("Vertical");
 
-        if (!_gameManager.gameOver)
+        if (!gameManager.gameOver)
         {
             transform.Translate(Vector3.forward * _speed * Time.deltaTime * _verticalInput);
             transform.Translate(Vector3.right * _speed * Time.deltaTime * _horizontalInput);
@@ -31,6 +34,11 @@ public class PlayerController : MonoBehaviour
             if (Input.GetKeyDown(KeyCode.Space))
             {
                 Instantiate(projectilePrefab, transform.position, transform.rotation);
+                _state = 4;
+            }
+            else
+            {
+                _state = 0;
             }
 
             if (transform.position.x > _horizontalBounds)
@@ -53,6 +61,20 @@ public class PlayerController : MonoBehaviour
                 transform.position = new Vector3(transform.position.x, transform.position.y, _lowerBound);
             }
         }
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
+        {
+            _state = 1;
+        }
+        else
+        {
+            _state = 0;
+        }
+        /*if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
+        {
+
+        }*/
+
+        animator.SetInteger("state", _state);
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -60,6 +82,8 @@ public class PlayerController : MonoBehaviour
         if (!collision.gameObject.CompareTag("Ground") & !collision.gameObject.CompareTag("Projectile"))
         {
             Destroy(collision.gameObject);
+
+    
         }
     }
 }
