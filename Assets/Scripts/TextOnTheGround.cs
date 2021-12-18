aa7e100135643eae782e33eb244d3870597daa86
MarudinArthur <85170410+MarudinArthur@users.noreply.github.com>
2021-12-18 23:30:58 +0300

    minor fix text on the ground animation

--- Assets/Scripts/TextOnTheGround.cs
+++ Assets/Scripts/TextOnTheGround.cs
@@ -6,21 +6,30 @@ public class TextOnTheGround : MonoBehaviour
 {
     public TMP_Text textOnTheGround;
     private float speed = 1.5f;
+    private GameManager _gameManager;
+
+    private void Start()
+    {
+        _gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+    }
 
     private void Update()
     {
         StartCoroutine("ShowTextOnTheGround");
-
-        if(textOnTheGround.transform.position.z > 15)
-        {
-            Destroy(gameObject);
-        }
+        if (textOnTheGround.transform.position.z > 15)
+            {
+                Destroy(gameObject);
+            }
     }
 
     IEnumerator ShowTextOnTheGround()
     {
         yield return new WaitForSeconds(3);
-        textOnTheGround.GetComponent<MeshRenderer>().enabled = true;
-        textOnTheGround.transform.Translate(Vector3.up * speed * Time.deltaTime);
+
+        if (!_gameManager.stopGame)
+        {
+            textOnTheGround.GetComponent<MeshRenderer>().enabled = true;
+            textOnTheGround.transform.Translate(Vector3.up * speed * Time.deltaTime);
+        }
     }
 }
