9b37e87579d63087ed33df066ffee67cea4120b9
MarudinArthur <85170410+MarudinArthur@users.noreply.github.com>
2022-01-11 23:56:52 +0300

    add weapon script

---
+++ Assets/Scripts/Weapons.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapons : MonoBehaviour
+{
+    private float fireRate; //скорость стрельбы
+    private float range; // дальность
+    private float damage; //урон
+    private float reload; //перезарядка
+    private float spread; //разброс стрельбы
+    private float clip; //обойма
+
+
+}
+
+public class ShootGun : Weapons
+{
+   
+}
